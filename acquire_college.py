d9f50b885c175a04a5359538943c64aea490bf5a
RaulCPena <raul.c.pena@gmail.com>
2020-04-24 08:16:19 -0500

    deleted acquire_college.py file because it was reading in the wrong data

--- acquire_college.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# df_scorecard = pd.read_csv('https://query.data.world/s/zxrfcjsdszuwmcm2y7svsm4ttcgfcf', encoding="iso-8859-1")
-# df_scorecard.to_csv("scorecard.csv")
-
-
-df_completion = pd.read_csv('https://query.data.world/s/hqu3fnobwppzcjmjxldhlg3bdjyrrh', encoding="iso-8859-1")
-df_completion.to_csv("completion.csv")
